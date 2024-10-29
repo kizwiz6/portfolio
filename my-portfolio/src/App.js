5d36faecfc266ec3752d717203b13913b618213e
Kieran Emery <kizwiz@hotmail.co.uk>
2024-10-29 19:31:27 +0000

    Refactor routing in App.js and Portfolio.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // Import Navigate
 import Portfolio from './pages/Portfolio';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -16,7 +17,7 @@ function App() {
       <div className="App">
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<Portfolio experiences={experiences} />} />
+          <Route path="/" element={<Navigate to="/about" />} /> {/* Redirect base path to /about */}
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
--- my-portfolio/src/pages/Portfolio.js
+++ my-portfolio/src/pages/Portfolio.js
@@ -1,28 +1,31 @@
 // src/pages/Portfolio.js
 
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom'; // Import useLocation
 import Layout from '../components/Layout';
-import About from '../pages/About';
-import Projects from '../pages/Projects';
-import Experience from '../pages/Experience';
-import Education from '../pages/Education';
+import About from './About';
+import Projects from './Projects';
+import Experience from './Experience';
+import Education from './Education';
 
 const Portfolio = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [activeTabState, setActiveTab] = useState('projects'); // Default to 'projects'
+  
+  // Get the current path
+  const location = useLocation();
+  const activeTabState = location.pathname.split('/').pop(); // Get the last part of the path
 
   return (
-    <Layout activeTab={activeTabState} setActiveTab={setActiveTab} setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen}>
+    <Layout activeTab={activeTabState} setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen}>
       {/* Main Content */}
       <main className="max-w-6xl mx-auto px-4 py-8">
-        {activeTabState === 'about' && <About activeTab={activeTabState} setActiveTab={setActiveTab} setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />}
-        {activeTabState === 'projects' && <Projects activeTab={activeTabState} setActiveTab={setActiveTab} setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />}
-        {activeTabState === 'experience' && <Experience activeTab={activeTabState} setActiveTab={setActiveTab} setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />}
-        {activeTabState === 'education' && <Education activeTab={activeTabState} setActiveTab={setActiveTab} setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />}
+        {activeTabState === 'about' && <About />}
+        {activeTabState === 'projects' && <Projects />}
+        {activeTabState === 'experience' && <Experience />}
+        {activeTabState === 'education' && <Education />}
       </main>
     </Layout>
   );
 };
 
-// Exporting the Portfolio (default export) component
 export default Portfolio;
